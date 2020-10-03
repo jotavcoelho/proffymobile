c3f64170a45096d3e12e4be003eb6947fe18e4b1
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-10-03 13:28:45 -0300

    Teach page fully styled and functional

--- src/pages/Teach/index.js
+++ src/pages/Teach/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import teachBgImage from '../../assets/images/teach-background.png';
+
+import styles from './styles';
 
 function Teach() {
-  return <View />
+  const { goBack } = useNavigation();
+
+  function handleGoBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground 
+        source={teachBgImage} 
+        style={styles.content}
+        resizeMode="contain"
+      >
+        <Text style={styles.title}>Wanna be a Proffy?</Text>
+        <Text style={styles.description}>
+          To get started, you gotta sign up as a teacher on our website.
+        </Text>
+      </ImageBackground>
+      
+      <RectButton onPress={handleGoBack} style={styles.okButton} >
+        <Text style={styles.okButtonText} >Okay</Text>
+      </RectButton>
+
+    </View>
+  )
 }
 
 export default Teach;
