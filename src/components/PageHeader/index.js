74d3c0473834764ced3cada72d136efc761a3f14
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-10-03 21:48:13 -0300

    PageHeader styled and functional

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import backIcon from '../../assets/images/icons/back.png';
+import logoImg from '../../assets/images/logo.png';
 
 import styles from './styles';
 
-function PageHeader() {
+function PageHeader({ title }) {
+  const { navigate } = useNavigation();
+
+  function handleGoBack() {
+    navigate('Landing');
+  }
+
   return (
-    <View>
-      
+    <View style={styles.container} >
+      <View style={styles.topBar} >
+        <BorderlessButton onPress={handleGoBack} >
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode="contain" />
+      </View>
+
+      <Text style={styles.title}>
+        {title}
+      </Text>
     </View>
   )
 }
