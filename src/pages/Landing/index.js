205812dc68bbd8a7616a4b97c59d752d1a4e7aed
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-10-05 11:31:21 -0300

    Added and now using axios, 'dynamized' total connections on landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -12,6 +13,16 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 const Landing = () => {
   const { navigate } = useNavigation();
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+      
+      setTotalConnections(total);
+    });
+  }, []);
 
   function handleNavigateToTeachPage() {
     navigate('Teach');
@@ -49,7 +60,7 @@ const Landing = () => {
     </View>
 
     <Text style={styles.totalConnections}>
-      Total of 727 connections made {' '}
+      Total of {totalConnections} connections made {' '}
       <Image source={heartIcon} />
     </Text>
   </View> ;
